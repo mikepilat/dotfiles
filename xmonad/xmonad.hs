9f13039ab3f9bb6feb4a0dcf3e623acc2dfe84d4
Michael Pilat <mike@mikepilat.com>
2017-04-25 00:16:45 -0500

    Improved xmonad config and add xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,30 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import System.IO
+import Graphics.X11.ExtraTypes.XF86
+import qualified Data.Map as M
+import XMonad.Util.EZConfig ( additionalKeys )
 
-main = xmonad def
- 	{ terminal = "urxvt"
-	, modMask = mod4Mask
-	, borderWidth = 3
-	}
+main = do
+  xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
+  xmonad $ docks defaultConfig
+	  { terminal = "urxvt"
+	  , modMask = mod4Mask
+	  , borderWidth = 2
+	  , normalBorderColor = "#cccccc"
+	  , focusedBorderColor = "#ff8800"
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , logHook = dynamicLogWithPP xmobarPP
+      { ppOutput = hPutStrLn xmproc
+      , ppTitle = xmobarColor "blue" "" . shorten 50
+      }
+    , keys = myKeys
+	  }
+
+myKeys (XConfig {modMask = mod4}) = M.fromList
+    [ ((0, xF86XK_AudioLowerVolume   ), spawn "amixer set Master 2-")
+    , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer set Master 2+")
+    , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle") ]
